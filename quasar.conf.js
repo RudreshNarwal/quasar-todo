299c9716d4b77d005e31c609049e6085d830e53f
Sarvex Jatasra <sarvex.jatasra@gmail.com>
2019-09-02 14:13:22 +0530

    Add new Todo item

--- quasar.conf.js
+++ quasar.conf.js
@@ -42,16 +42,27 @@ module.exports = function (ctx) {
         'QTabs',
         'QTab',
         'QRouteTab',
-        'QCheckbox'
+        'QCheckbox',
+        'QDialog',
+        'QCard',
+        'QCardSection',
+        'QCardActions',
+        'QSpace',
+        'QInput',
+        'QDate',
+        'QPopupProxy',
+        'QTime'
       ],
 
       directives: [
-        'Ripple'
+        'Ripple',
+        'ClosePopup'
       ],
 
       // Quasar plugins
       plugins: [
-        'Notify'
+        'Notify',
+        'Dialog'
       ]
 
       // iconSet: 'ionicons-v4'
